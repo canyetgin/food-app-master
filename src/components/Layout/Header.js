4d352554b17426d0153105816f0c895354e95a77
ozkankutuk <ozkan@gemas.com.tr>
2023-01-24 08:43:48 +0300

    Cart component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,12 @@
 import classes from './Header.module.css';
 import img from '../../assests/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <button>Cart</button>
+        <HeaderCartButton/>
       </header>
       <div className={classes['main-image']}>
         <img src={img} alt='meals' />
