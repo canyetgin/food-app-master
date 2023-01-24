62f3e589b3e5df03be480b0379dab286a0fd0c07
ozkankutuk <ozkan@gemas.com.tr>
2023-01-24 15:46:20 +0300

    context actions

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,11 +1,33 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+    const updated = state.items.concat(action.item);
+    const newTotal = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updated,
+      totalAmount: newTotal,
+    };
+  }
+};
+
 const CartProvider = (props) => {
-  const addItem = (item) => {};
-  const removeItem = (item) => {};
+  const [cartState, dispatchCart] = useReducer(cartReducer, defaultCartState);
+  const addItem = (item) => {
+    dispatchCart({ type: 'ADD_ITEM', item: item });
+  };
+  const removeItem = (item) => {
+    dispatchCart({ type: 'REMOVE_ITEM', item: item });
+  };
   const context = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItem,
     removeItem: removeItem,
   };
