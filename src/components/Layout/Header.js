c47d93c166077798702b70704d2b67b38081f9b0
ozkankutuk <ozkan@gemas.com.tr>
2023-01-24 14:19:28 +0300

    UI reusable comp, Meals

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,20 @@
-import classes from './Header.module.css';
-import img from '../../assests/meals.jpg';
+import { Fragment } from 'react';
+
 import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
 const Header = (props) => {
   return (
-    <>
+    <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton/>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
-        <img src={img} alt='meals' />
+        <img src={mealsImage} alt='A table full of delicious food!' />
       </div>
-    </>
+    </Fragment>
   );
 };
 
