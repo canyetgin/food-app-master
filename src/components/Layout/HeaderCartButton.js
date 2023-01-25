95ebcdc8dd88f3139cbbab11fe80a6082f030686
ozkankutuk <ozkan@gemas.com.tr>
2023-01-25 16:50:05 +0300

    cart btn bump effect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,28 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButton] = useState(false);
   const ctx = useContext(CartContext);
   const numberOfItems = ctx.items.reduce((acc, item) => {
     return acc + item.amount;
   }, 0);
+  const { items } = ctx;
+  const btnClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ''
+  }`;
+  useEffect(() => {
+    if (ctx.items.length === 0) return;
+    setButton(true);
+    const timer = setTimeout(() => setButton(false), 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
