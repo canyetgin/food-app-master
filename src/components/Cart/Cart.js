ec2a4d1586e26e114ba9f77f4e136ae734defeb2
ozkankutuk <ozkan@gemas.com.tr>
2023-01-25 16:39:11 +0300

    bug fixes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,14 +2,30 @@ import { useContext } from 'react';
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
-
+import CartItem from './CartItem';
 const Cart = (props) => {
   const ctx = useContext(CartContext);
   const totalAmount = `$${ctx.totalAmount.toFixed(2)} `;
+  const hasItems = ctx.items.length > 0;
+
+  const cartItemRemove = (id) => {
+    ctx.removeItem(id);
+  };
+  const cartItemAdd = (item) => {
+    ctx.addItem(item);
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {ctx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onRemove={cartItemRemove.bind(null, item.id)}
+          onAdd={cartItemAdd.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -25,7 +41,7 @@ const Cart = (props) => {
         <button className={classes['button--alt']} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
